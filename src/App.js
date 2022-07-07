2f40527191d35a10e25fee48984da7e44bdd80a6
Leo <leodgrim117@gmail.com>
2022-07-06 22:28:58 -0500

    Initial commit. Login page code location in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import React from 'react';
+import './login.css'
+
+class login extends React.Component
+{
+  constructor()
+  {
+      super();
+      this.state = {email: '', pass: ''}
+  }
+
+  //Runs events that occur after button is pressed
+  CollectInfo()
+  {
+    console.log('log');
+  }
+
+  render()
+  {
+    return(
+      <div className='loginSquare'>
+        <h1 className='loginElement'>Login</h1>
+
+        <TextField className='loginElement' id="email-form"  label="Email" variant="outlined"></TextField> <br></br>
+        <TextField className='loginElement' id="password-form" label="password" type="password" variant="outlined"></TextField> <br></br>
+
+        <Button variant="contained" className='loginElement' onClick={this.CollectInfo}>
+          Login
+        </Button>
+
+        {/*Add link to <a> tag that goes to create account*/}
+        <a className='loginElement' href="https://www.youtube.com/">New User? Click Here to Create Account.</a>
+      </div>
+    )
+  }
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const loginInstance = new login();
+  return(
+  <div>
+    Main page
+    <Button variant="contained" className='OpenLogin'>Login</Button>
+    {loginInstance.render()}
+  </div>
+  )
 }
 
 export default App;
