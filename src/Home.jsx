71f8e68af7247aae2aa355164475cd9a473cf3a3
David <davidpkeeter@gmail.com>
2022-07-09 17:20:09 -0500

    moved login button to navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,23 +5,11 @@ import Login from './Login'
 
 
 function Home(){
-    const [loginOpen, setLoginOpen] = useState(false);
-    const [OtherOpen, setOtherOpen] = useState(false);
+   
     return(
     
     <div>
-        <body>
-    Main page
-    <Button variant="contained" className='OpenLogin' onClick={() => setLoginOpen(true)}>
-    Login
-    </Button>
-    <Button variant="contained" className='OpenOther' onClick={() => setOtherOpen(true)}>
-    Other
-    </Button>
-    
-    <Modal open={loginOpen} onClose={() => setLoginOpen(false)}><Login /></Modal>
-    <Modal open={OtherOpen} onClose={() => setOtherOpen(false)}>Test</Modal>
-    </body>
+ HELLO
     </div>
     )
 }
