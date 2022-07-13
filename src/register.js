b3c9c6c7119bfaa10081439b8143b651994456ff
Harr, Evan <evanharr@my.unt.edu>
2022-07-12 19:54:44 -0500

    changing firebase import

--- src/register.js
+++ src/register.js
@@ -12,15 +12,14 @@ import Modal from './Modal'
 
 
  // Import the functions you need from the SDKs you need
- import {initializeApp} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+ import {initializeApp} from "firebase/app";
  import {
      getAuth,
      createUserWithEmailAndPassword,
      signInWithEmailAndPassword,
      signOut
- } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
- import {getDatabase, set, ref, update} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
-
+ } from "firebase/auth";
+import {getDatabase, set, ref, update} from "firebase/database";
 
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
