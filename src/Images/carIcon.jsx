5ab2611fc2ab26fe0e0729d2692bd73cc4ed18a5
David <davidpkeeter@gmail.com>
2022-07-31 12:36:37 -0500

    adding accesibility labels

--- src/Images/carIcon.jsx
+++ src/Images/carIcon.jsx
@@ -3,7 +3,7 @@ import carIconLogo from './carIconLogo.svg'
 
 function carIcon(){
     return(
-        <img src={carIconLogo} width='60px' height='47px'/>
+        <img src={carIconLogo} width='60px' height='47px' alt = "white outline of car and carkey"/>
     )
 }
 export default carIcon
